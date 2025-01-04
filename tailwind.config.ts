fce3b07dba83a6755825248d2bdc475e5c000635
Abdullah Hassouna <abood211957@outlook.com>
2025-01-04 13:32:05 +0200

    fix the global and the theme css files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./public*.{css,sass,scss}",
   ],
   theme: {
     extend: {
