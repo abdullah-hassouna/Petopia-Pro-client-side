68b76a5337d01100a0edb5606c982e0ad2114d08
Abdullah Hassouna <abood211957@outlook.com>
2025-01-04 13:32:05 +0200

    fix the global and the theme css files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-"use client"
+// "use client"
 
-import Head from 'next/head';
-// import type { Metadata } from "next";
-// import { Readex_Pro } from "next/font/google";
-// import "@/";
-// import "./themes.css";
+import type { Metadata } from "next";
+import { Readex_Pro } from "next/font/google";
+
+import "./globals.css";
+import "./themes.css";
 import ThemeToggle from './components/ThemeToggle';
 
-// const readexPro = Readex_Pro({
-// variable: "--font-readex-pro",
-//   subsets: ["latin"],
-// });
+const readexPro = Readex_Pro({
+  variable: "--font-readex-pro",
+  subsets: ["latin"],
+});
 
-// export const metadata: Metadata = {
-//   title: "Prtopia",
-//   description: "Prtopia Platform for animal Lovers",
-//   authors: [
-//     { name: "Abdullah Hassouna", url: "https://www.github.com/abdullah-hassouna" },
-//     { name: "Haitham Abu-Lumdi", url: "https://www.github.com/haitham-akram" }
-//   ],
-// };
+export const metadata: Metadata = {
+  title: "Prtopia",
+  description: "Prtopia Platform for animal Lovers",
+  authors: [
+    { name: "Abdullah Hassouna", url: "https://www.github.com/abdullah-hassouna" },
+    { name: "Haitham Abu-Lumdi", url: "https://www.github.com/haitham-akram" }
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -28,12 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="stylesheet" href="/globals.css" />
-        <link rel="stylesheet" href="/themes.css" />
-      </Head>
       <body
-      // className={`${readexPro.className} antialiased`}
+        className={`${readexPro.className} antialiased`}
       >
         <header className="p-4 bg-background">
           <div className="container mx-auto flex justify-end">
