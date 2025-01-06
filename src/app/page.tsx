ed140eaea37e92017d887b3835800e57b7bbe2d2
haitham-akram <hayhtam14@gmail.com>
2025-01-06 16:47:14 +0200

    still bugged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { Button } from "@/components/ui/button";
 
 function Home() {
   return (
+    <div>
     <h1 className="text-3xl font-bold">Welcom Back, Mohammad!</h1>
+
+   <div className="flex gap-4 mt-4">
+   {/* <Button >primary</Button>
+   <Button variant="destructive">primary</Button> */}
+
+    <Button variant={"product"} size="lg">product</Button>
+    <Button variant={"adoption"}>adoption</Button>
+    <Button variant={"discuss"}>discuss</Button>
+    <Button variant={"sale"}>sale</Button>
+    <Button variant={"help"}>help</Button>
+    <Button variant={"ghost"}>ghost</Button>
+   </div>
+
+
+    </div>
   );
 }
 
 
-export default Home
+export default Home 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,10 +11,48 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: {
+          DEFAULT: 'var(--prime-color)',
+          50: 'rgba(var(--prime-color-rgb), 0.5)',
+          75: 'rgba(var(--prime-color-rgb), 0.75)',
+          90: 'rgba(var(--prime-color-rgb), 0.9)',
+        },
+        adoption: {
+          DEFAULT: 'var(--adoption-tag)',
+          90: 'rgba(var(--adoption-tag-rgb), 0.9)',
+        },
+        product: {
+          DEFAULT: 'var(--product-tag)',
+          90: 'rgba(var(--product-tag-rgb), 0.9)',
+        },
+        discuss: {
+          DEFAULT: 'var(--discuss-tag)',
+          90: 'rgba(var(--discuss-tag-rgb), 0.9)',
+        },
+        sale: {
+          DEFAULT: 'var(--sale-tag)',
+          90: 'rgba(var(--sale-tag-rgb), 0.9)',
+        },
+        help: {
+          DEFAULT: 'var(--help-tag)',
+          90: 'rgba(var(--help-tag-rgb), 0.9)',
+        },
       },
-    },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
   },
-  plugins: [],
+  safelist: [
+    'hover:bg-adoption/90',
+    'hover:bg-product/90',
+    'hover:bg-discuss/90',
+    'hover:bg-sale/90',
+    'hover:bg-help/90',
+  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
