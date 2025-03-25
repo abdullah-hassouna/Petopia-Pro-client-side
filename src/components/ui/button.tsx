c31c9d7ccaad084c3c38e485d633b318d43ed870
abdullah-hassouna <abood211957@outlook.com>
2025-03-25 10:49:54 +0200

    adding the disablility for buttons in loading state of the website

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,8 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/lib/reduxStore/store'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-zinc-300 font-thin',
@@ -49,7 +51,11 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+
+    const isLoading = useSelector((state: RootState) => state.loading.isLoading)
+
+
+    return <Comp disabled={isLoading} className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
   }
 )
 Button.displayName = 'Button'
