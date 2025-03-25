76869c2a87c31eca949442846c61f681eea58132
abdullah-hassouna <abood211957@outlook.com>
2025-03-25 13:48:35 +0200

    Adding Skeleton to the posts and messages

--- src/app/components/Post/PostsContainer.tsx
+++ src/app/components/Post/PostsContainer.tsx
@@ -1,17 +1,26 @@
+"use client"
+
 import { ScrollArea } from '@/components/ui/scroll-area'
 import PostCard from './PostCard'
+import PostSkeleton from '@/app/skeleton/PostSkeleton'
+import { useState } from 'react'
 
 interface Posts {
   tag: 'adoption' | 'help' | 'discuss' | 'product' | 'other'
 }
 
 const PostContainer = ({ posts }: { posts: Posts[] }) => {
+
+  const [dataReady, setDataReady] = useState<boolean>(false)
+
+  setTimeout(() => setDataReady(() => true), 5000)
+
   return (
     <div className="w-full px-2 sm:px-3 md:px-4 lg:px-5 pb-5 h-screen">
       <ScrollArea id="container" className="w-full h-full overflow-auto">
         <div className="flex flex-col gap-4 md:gap-6">
           {posts.map((post, index) => (
-            <PostCard title={post.tag} key={index} />
+            dataReady ? <PostCard title={'adoption'} key={index} {...post} /> : <PostSkeleton key={index} />
           ))}
         </div>
       </ScrollArea>
